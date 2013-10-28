be2ac93290d21b9b9da93d294221f25c7295f662
Themaister <maister@archlinux.us>
2013-10-28 14:40:57 +0100

    Fixups for noise synth.

--- synth.cpp
+++ synth.cpp
@@ -2,11 +2,17 @@
 #include <cmath>
 #include <algorithm>
 
+#include "flute_iir_300.h"
+
 using namespace std;
 
 AirSynth::AirSynth(const char *device)
 {
+   //fm_tones.resize(32);
    tones.resize(32);
+   for (auto &tone : tones)
+      tone.set_filter_bank(&filter_bank);
+
    sustain.resize(16);
    audio = unique_ptr<AudioDriver>(new ALSADriver(device, 44100, 1));
    dead.store(false);
@@ -43,7 +49,7 @@ void AirSynth::set_note(unsigned channel, unsigned note, unsigned velocity)
    else
    {
       auto itr = find_if(begin(tones), end(tones),
-            [](const FM &t) { return !t.active->load(); });
+            [](const NoiseIIR &t) { return !t.active->load(); });
 
       if (itr == end(tones))
       {
@@ -145,9 +151,8 @@ void AirSynth::FM::render(float *out, unsigned samples)
    fill(out + i, out + samples, 0.0f);
 }
 
-void AirSynth::FM::reset(unsigned channel, unsigned note, unsigned vel)
+void AirSynth::Synth::reset(unsigned channel, unsigned note, unsigned vel)
 {
-   float omega = 2.0 * M_PI * 440.0 * pow(2.0, (note - 69.0) / 12.0) / 44100.0;
    velocity = vel / 127.0f;
 
    released = false;
@@ -156,7 +161,12 @@ void AirSynth::FM::reset(unsigned channel, unsigned note, unsigned vel)
    this->channel = channel;
 
    time = 0.0;
+   active->store(vel != 0);
+}
 
+void AirSynth::FM::reset(unsigned channel, unsigned note, unsigned vel)
+{
+   float omega = 2.0 * M_PI * 440.0 * pow(2.0, (note - 69.0) / 12.0) / 44100.0;
    carrier = {};
    modulator = {};
    carrier.omega = omega;
@@ -174,7 +184,85 @@ void AirSynth::FM::reset(unsigned channel, unsigned note, unsigned vel)
    modulator.env.release = 0.1;
    modulator.env.gain = 2.5;
 
-   active->store(vel != 0);
+   Synth::reset(channel, note, vel);
+}
+
+void AirSynth::NoiseIIR::reset(unsigned channel, unsigned note, unsigned vel)
+{
+   iir_ptr = 0;
+   iir_buffer.clear();
+   iir_len = sizeof(flute_iir_300) / sizeof(flute_iir_300[0]);
+   iir_buffer.resize(2 * iir_len);
+
+   history.clear();
+   history_ptr = 0;
+
+   float offset = note - (69.0f + 7.0f);
+   decimate_factor = round(interpolate_factor * pow(2.0f, offset / 12.0f));
+   phase = 0;
+
+   env.attack = 0.035;
+   env.delay = 0.035;
+   env.sustain_level = 0.35;
+   env.release = 0.8;
+   env.gain = exp(0.015 * (69.0 - note));
+
+   Synth::reset(channel, note, vel);
+}
+
+void AirSynth::NoiseIIR::render(float *out, unsigned samples)
+{
+   unsigned s;
+   for (s = 0; s < samples; s++, phase += decimate_factor)
+   {
+      if (released && time >= released_time + env.release)
+      {
+         sustained = false;
+         released = false;
+         active->store(false);
+         //fprintf(stderr, "Ended note %u.\n", note);
+         break;
+      }
+
+      while (phase >= interpolate_factor)
+      {
+         history_ptr = history_ptr ? history_ptr - 1 : history_len - 1;
+         history[history_ptr] = history[history_ptr + history_len] = noise_step(); 
+         phase -= interpolate_factor;
+      }
+
+      const float *filter = bank->buffer.data() + phase * bank->taps;
+      const float *src = history.data() + history_ptr;
+      float res = 0.0f;
+      for (unsigned i = 0; i < history_len; i++)
+         res += filter[i] * src[i];
+
+      out[s] = env.envelope(time, released) * res;
+      time += time_step;
+   }
+
+   fill(out + s, out + samples, 0.0f);
+}
+
+float AirSynth::NoiseIIR::noise_step()
+{
+   float res = dist(engine);
+   const float *src = iir_buffer.data() + iir_ptr;
+   for (unsigned i = 0; i < iir_len; i++)
+      res -= src[i] * flute_iir_300[i];
+
+   iir_ptr = iir_ptr ? iir_ptr - 1 : iir_len - 1;
+   iir_buffer[iir_ptr] = iir_buffer[iir_ptr + iir_len] = res;
+   return res;
+}
+
+void AirSynth::NoiseIIR::set_filter_bank(const PolyphaseBank *bank)
+{
+   this->bank = bank;
+   interpolate_factor = bank->phases;
+   history_len = bank->taps;
+   history.clear();
+   history.resize(2 * history_len);
 }
 
 double AirSynth::Envelope::envelope(double time, bool released)
@@ -209,5 +297,31 @@ double AirSynth::Oscillator::step(Oscillator &osc, double depth)
    double ret = sin(angle);
    angle += omega + depth * osc.omega * osc.step();
    return ret;
+}
+
+double AirSynth::PolyphaseBank::sinc(double v) const
+{
+   if (fabs(v) < 0.00001)
+      return 1.0;
+   else
+      return sin(v) / v;
+}
+
+AirSynth::PolyphaseBank::PolyphaseBank(unsigned taps, unsigned phases)
+   : taps(taps), phases(phases)
+{
+   buffer.resize(taps * phases);
+   std::vector<float> tmp(taps * phases);
+   int elems = taps * phases;
+   for (int i = 0; i < elems; i++)
+   {
+      double phase = M_PI * double(i - (elems >> 1)) / phases;
+      double window_phase = 2.0 * phase / taps;
+      tmp[i] = 0.85 * sinc(0.85 * phase) * sinc(window_phase);
+   }
+
+   for (unsigned t = 0; t < taps; t++)
+      for (unsigned p = 0; p < phases; p++)
+         buffer[p * taps + t] = tmp[t * phases + p];
 }
 
--- synth.hpp
+++ synth.hpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <cstdint>
 #include <vector>
+#include <random>
 #include "audio_driver.hpp"
 
 class Synthesizer
@@ -58,31 +59,73 @@ class AirSynth : public Synthesizer
          double step(Oscillator &osc, double depth);
       };
 
-      struct FM 
+      struct Synth
       {
-         FM() { reset(0, 0, 0); }
-
-         std::unique_ptr<std::atomic_bool> active{new std::atomic_bool};
-         unsigned note;
-         unsigned channel;
-         double time;
-
+         Synth() { active->store(false); }
+         unsigned note = 0;
+         unsigned channel = 0;
+         double time = 0;
          double time_step = 1.0 / 44100.0;
-         float velocity;
-
-         Oscillator carrier, modulator;
-
          double released_time = 0.0;
 
-         bool sustained;
-         bool released;
+         bool sustained = false;
+         bool released = false;
+         float velocity = 0.0f;
          std::unique_ptr<std::mutex> lock{new std::mutex};
+         std::unique_ptr<std::atomic_bool> active{new std::atomic_bool};
 
-         void render(float *out, unsigned samples);
-         void reset(unsigned channel, unsigned note, unsigned velocity);
+         virtual void render(float *out, unsigned samples) = 0;
+         virtual void reset(unsigned channel, unsigned note, unsigned velocity);
       };
 
-      std::vector<FM> tones;
+      struct PolyphaseBank
+      {
+         PolyphaseBank(unsigned taps, unsigned phases);
+         std::vector<float> buffer;
+         unsigned taps;
+         unsigned phases;
+
+         double sinc(double v) const;
+      };
+      PolyphaseBank filter_bank{8, 1 << 13};
+
+      struct NoiseIIR : Synth
+      {
+         Envelope env;
+         std::vector<float> iir_buffer;
+         unsigned iir_ptr = 0;
+         unsigned iir_len = 0;
+
+         void render(float *out, unsigned samples) override;
+         void reset(unsigned channel, unsigned note, unsigned velocity) override;
+
+         unsigned interpolate_factor = 0;
+         unsigned decimate_factor = 0;
+         unsigned phase = 0;
+         std::vector<float> history;
+         unsigned history_ptr = 0;
+         unsigned history_len = 0;
+
+         float noise_step();
+
+         void set_filter_bank(const PolyphaseBank *bank);
+         const PolyphaseBank *bank = nullptr;
+
+         std::default_random_engine engine;
+         std::uniform_real_distribution<float> dist{-0.01f, 0.01f};
+      };
+
+      struct FM : Synth
+      {
+         Oscillator carrier, modulator;
+
+         void render(float *out, unsigned samples) override;
+         void reset(unsigned channel, unsigned note, unsigned velocity) override;
+      };
+
+      //std::vector<FM> fm_tones;
+      std::vector<NoiseIIR> tones;
+
       std::vector<bool> sustain;
       std::atomic<bool> dead;
 
